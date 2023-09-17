088ddeaa0ca586d4561e9ba0ef285a10accf34ec
Airlex <contact@airlex.me>
2023-09-17 21:22:48 +0400

    feat: add numbers to string

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import random
 
-def generateString(lenght, upper_letter):
+def generateString(lenght, upper_letter, numbers):
     string = ""
 
     for i  in range(0, lenght):
+        num_rate = random.randint(0, 4)
         if (upper_letter) == True:
-            string += chr(random.randint(ord('A'), ord('Z')))
+            if (numbers == True and num_rate == 1):
+                string += str(random.randint(0, 9))
+            else:
+                string += chr(random.randint(ord('A'), ord('Z')))
         else:
-            string += chr(random.randint(ord('a'), ord('z')))
+            if (numbers == True and num_rate == 1):
+                string += str(random.randint(0, 9))
+            else:
+                string += chr(random.randint(ord('a'), ord('z')))
     return string
 
 input_lenght = int(input("Lenght: "))
-input_upper_letter = input("Upper Letter (y/n): ")
 
+input_upper_letter = input("Upper Letter (y/n): ")
 if (input_upper_letter == "y"):
     input_upper_letter = True
 else:
     input_upper_letter = False
 
-print(generateString(input_lenght, input_upper_letter))
+input_numbers = input("Numbers (y/n): ")
+if (input_numbers == "y"):
+    input_numbers = True
+else:
+    input_numbers = False
+
+print(generateString(input_lenght, input_upper_letter, input_numbers))
