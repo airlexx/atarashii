7e540df893e06c1d04da3db3e66c921f367172db
Airlex <contact@airlex.me>
2023-09-30 23:08:31 +0400

    feat: add special characters

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ def welcome():
     print("by airlex\n")
     print("Simple password generator\n")
 
-def generateString(lenght, upper_letter, numbers):
+def generateString(lenght, upper_letter, numbers, special_char):
     string = ""
+    special_char_array = ['!','#','?','%', '*', '$']
 
     for i  in range(0, lenght):
         num_rate = random.randint(0, 4)
-        if (upper_letter) == True:
+        special_rate = random.randint(0, 6)
+        if (upper_letter):
             if (numbers == True and num_rate == 1):
                 string += str(random.randint(0, 9))
+            if (special_char and special_rate == 1):
+                string += str(random.choice(special_char_array))
             else:
                 string += chr(random.randint(ord('A'), ord('Z')))
         else:
             if (numbers == True and num_rate == 1):
                 string += str(random.randint(0, 9))
+            if (special_char and special_rate == 1):
+                string += str(random.choice(special_char_array))
             else:
                 string += chr(random.randint(ord('a'), ord('z')))
     return [True, string]
@@ -40,6 +46,9 @@ def userInput(mode: int, default: bool|int):
 
     if (mode == 2):
         entry = input("Do you want numbers? (Y/n): ").lower().strip()
+
+    if (mode == 3):
+        entry = input("Do you want special characters? (y/N): ").lower().strip()
 
     match entry:
         case "y":
@@ -65,4 +74,7 @@ if (r0[0] and r1[0]):
     r2 = userInput(2, True)
 
 if (r0[0] and r1[0] and r2[0]):
-    print("\nPassword: " + generateString(r0[1], r1[1], r2[1])[1])
+    r3 = userInput(3, False)
+
+if (r0[0] and r1[0] and r2[0] and r3[0]):
+    print("\nPassword: " + generateString(r0[1], r1[1], r2[1], r3[1])[1])
